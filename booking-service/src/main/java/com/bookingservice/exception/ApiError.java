a47dfae519ff3166f5144c3dcd971563ee9ba5ae
Nandharamshiva <shivashankarnandharam@gmail.com>
2026-03-18 16:08:37 +0530

    updated booking service

---
+++ booking-service/src/main/java/com/bookingservice/exception/ApiError.java
@@ -0,0 +1,13 @@
+package com.bookingservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiError {
+
+    private int status;
+    private String message;
+    private long timestamp;
+}
